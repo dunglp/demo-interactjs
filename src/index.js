cecbbeace28ce4160d35a26bc296703767622b18
Bi <dunglp1303@gmail.com>
2019-02-26 10:24:22 +0700

    Added : enable resize for dropzone
    Added :
    - enable resize for dropzone

--- src/index.js
+++ src/index.js
@@ -72,4 +72,41 @@ interact('.dropzone')
             event.target.classList.remove('drop-active');
             event.target.classList.remove('drop-target');
         }
+    }).resizable({
+        // resize from all edges and corners
+        edges: { 
+            left: false, 
+            right: false, 
+            bottom: true, 
+            top: false 
+        },
+
+        // keep the edges inside the parent
+        restrictEdges: {
+            outer: 'parent',
+            endOnly: true,
+        },
+
+        inertia: true,
+    })
+    .on('resizemove', function (event) {
+        var target = event.target,
+            x = (parseFloat(target.getAttribute('data-x')) || 0),
+            y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+        // update the element's style
+        target.style.width  = event.rect.width + 'px';
+        target.style.height = event.rect.height + 'px';
+        // translate when resizing from top or left edges
+        x += event.deltaRect.left;
+        y += event.deltaRect.top;
+
+        target.style.webkitTransform = target.style.transform =
+            'translate(' + x + 'px,' + y + 'px)';
+
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+        target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height);
+    }).on('resizeend', function (event) {
+        event.target.textContent = '';
     });
